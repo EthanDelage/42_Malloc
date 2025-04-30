f37eb68c572558ad0246cfd3fc4c886d0d35bf42
Ethan Delage <ethandelage04@gmail.com>
2025-04-30 15:21:05 +0200

    feat: add basic struct
    malloc_data_t it used to globally store the mapped zones
    
    page_header_t is a linked list used to store a map's allocated pointer
    
    chunk_header_t is a doubly linked list that stores allocation data

--- include/malloc.h
+++ include/malloc.h
@@ -3,6 +3,35 @@
 
 #include <stdlib.h>
 
+#define TINY_MAX_SIZE 64
+#define SMALL_MAX_SIZE 512
+#define ZONE_CHUNK_MIN_COUNT 100
+
+typedef struct malloc_data_s malloc_data_t;
+typedef struct page_header_s page_header_t;
+typedef struct chunk_header_s chunk_header_t;
+
+struct malloc_data_s {
+    page_header_t *tiny;
+    page_header_t *small;
+    chunk_header_t *large;
+};
+
+struct page_header_s {
+    chunk_header_t *alloc_list;
+    chunk_header_t *free_list;
+    page_header_t *next;
+};
+
+struct chunk_header_s {
+    size_t size;
+    u_int8_t in_use;
+    chunk_header_t *next;
+    chunk_header_t *previous;
+};
+
+extern malloc_data_t malloc_data;
+
 void free(void *ptr);
 void *malloc(size_t size);
 void *realloc(void *ptr, size_t size);
--- src/malloc.c
+++ src/malloc.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
-
 #include "malloc.h"
+
+#include <stdio.h>
+malloc_data_t malloc_data = {0};
 
 void *malloc(size_t size) {
     (void) size;
