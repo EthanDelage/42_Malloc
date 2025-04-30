bc87743e8fa7120111f13cdce2d356504e2879c4
Ethan Delage <ethandelage04@gmail.com>
2025-05-01 00:40:34 +0200

    feat: add chunk_coalesce function

--- include/chunk.h
+++ include/chunk.h
@@ -6,4 +6,7 @@
 void chunk_dll_insert(chunk_header_t **pos, chunk_header_t *value);
 void chunk_dll_remove(chunk_header_t **pos);
 
+chunk_header_t *chunk_split(chunk_header_t *chunk, size_t size);
+void chunk_coalesce(chunk_header_t *chunk);
+
 #endif // CHUNK_H
--- src/chunk/chunk_utils.c
+++ src/chunk/chunk_utils.c
@@ -29,3 +29,26 @@ chunk_header_t *chunk_split(chunk_header_t *chunk, size_t size) {
     chunk_dll_insert(&chunk, new);
     return new;
 }
+
+/**
+ * @brief Coalesces the given chunk with its next adjacent chunk.
+ *
+ * This function merges the current chunk and its next contiguous chunk into a
+ * single larger free chunk. It assumes that both chunks are free and that the
+ * next chunk exists and is adjacent in memory.
+ *
+ * After coalescing, the size of the current chunk is increased to include the
+ * size of the next chunk and its header, and the doubly-linked list is updated
+ * accordingly.
+ *
+ * @param chunk The starting chunk to coalesce with its next neighbor.
+ */
+void chunk_coalesce(chunk_header_t *chunk) {
+    chunk_header_t *next = chunk->next;
+
+    chunk->next = next->next;
+    if (next->next != NULL) {
+        next->next->previous = chunk;
+    }
+    chunk->size += sizeof(chunk_header_t) + next->size;
+}
