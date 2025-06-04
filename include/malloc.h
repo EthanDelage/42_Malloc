6fd4d847876b6cb0f99163356800166085a15007
Ethan Delage <ethandelage04@gmail.com>
2025-06-04 23:01:05 +0200

    feat: implement calloc

--- include/malloc.h
+++ include/malloc.h
@@ -6,6 +6,7 @@
 __attribute__((visibility("default"))) void free(void *ptr);
 __attribute__((visibility("default"))) void *malloc(size_t size);
 __attribute__((visibility("default"))) void *realloc(void *ptr, size_t size);
+__attribute__((visibility("default"))) void *calloc(size_t nmemb, size_t size);
 __attribute__((visibility("default"))) void show_alloc_mem();
 
 #endif // MALLOC_H
---
+++ src/calloc.c
@@ -0,0 +1,21 @@
+#include "malloc.h"
+#include "utils/printf.h"
+
+#include <string.h>
+
+void *calloc(size_t nmemb, size_t size) {
+    void *ptr;
+    size_t total_size = nmemb * size;
+
+    printf("calloc\n");
+    if (nmemb != 0 && total_size / nmemb != size) {
+        return NULL;
+    }
+    ptr = malloc(total_size);
+    if (ptr != NULL) {
+        return NULL;
+    }
+    // TODO: remove libc function
+    memset(ptr, 0, total_size);
+    return ptr;
+}
