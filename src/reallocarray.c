2857b3727ddd9d614aa0bfcc8819637a1b7825e0
Ethan Delage <edelage@student.42lyon.fr>
2025-07-04 16:45:34 +0200

    fix: free ptr on size overflow in reallocarray

--- src/reallocarray.c
+++ src/reallocarray.c
@@ -1,12 +1,14 @@
 #include "malloc.h"
-#include "utils/printf.h"
 
+#include <errno.h>
 #include <stddef.h>
 
 void *reallocarray(void *ptr, size_t nmemb, size_t size) {
     size_t total_size = nmemb * size;
 
     if (nmemb != 0 && total_size / nmemb != size) {
+        free(ptr);
+        errno = ENOMEM;
         return NULL;
     }
     return realloc(ptr, total_size);
